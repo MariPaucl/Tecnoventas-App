810be7d9a944a04d9979c71e2d66c585a16e4d1b
Necron-01 <dypc.890@gmail.com>
2024-04-03 23:28:38 -0500

    Editar datos del cliente FrontEnd

--- tecnoventasApp/App.tsx
+++ tecnoventasApp/App.tsx
@@ -5,12 +5,14 @@ import { HomeScreen } from './src/views/Home/home';
 import { RegistroScreen } from './src/views/Registro/registro';
 import { LoginScreen } from './src/views/login/login';
 import PerfilScreen from "./src/views/PerfilUsuario/perfil";
+import { ActualizarScreen } from './src/views/Actualizar/actualizar';
 
 export type RootStackParamList = {
   HomeScreen: undefined;
   RegistroScreen: undefined;
   LoginScreen: undefined;
   PerfilScreen: undefined;
+  ActualizarScreen: undefined;
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
@@ -40,17 +42,22 @@ const App = () => {
             headerShown: true,
             title: "Registro",
           }} />
-           <Stack.Screen
+        <Stack.Screen
         name="PerfilScreen"
         component={PerfilScreen}
         options={{
           headerShown: true,
           title: "Perfil",
         }}/>
+        <Stack.Screen
+        name="ActualizarScreen"
+        component={ActualizarScreen}
+        options={{
+          headerShown: true,
+          title: "Actualizar",
+        }}/>
 
       </Stack.Navigator>
-
-
     </NavigationContainer>
   );
 };
---
+++ tecnoventasApp/src/views/Actualizar/ViewModel.tsx
@@ -0,0 +1,40 @@
+import React, {useState} from 'react';
+
+export const useViewModel = () => {
+    const [values, setValues] = useState({
+        nomCliente: '',
+        apeCliente: '',
+        fechaNac: '',
+        telefono: '',
+        correo: '',
+    });
+
+    const onChange = (property: string, value: any) => {
+        setValues({ ...values, [property]: value });
+    };
+
+    const update = async () => {
+        try {
+            const response = await fetch('http://172.29.240.1:3000/api/clientes/update', {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(values),
+            });
+            if (!response.ok) {
+                throw new Error('Error al actualizar datos del cliente');
+            }
+        } catch (error) {
+            throw error;
+        }
+    };
+
+    return {
+        ...values,
+        onChange,
+        update,
+    };
+};
+
+export default useViewModel;
